e0c2c6f0affd7c56028d408703eb2d2124522b06
Kaiqi Liang <kaiqi.liang@outlook.com>
2021-01-15 14:51:08 +1100

    Add text loop to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,33 @@
 import Background from '../components/Background';
+import TextLoop from 'react-text-loop';
+
+const details = {
+  first: [
+    'Programming',
+    'Software Engineering',
+    'Global Warming',
+  ],
+  second: [
+    'passion',
+    'pursuit',
+    'concern',
+  ],
+};
 
 export default () => {
   return (
     <Background>
-      <h1>Hi, my name is Kaiqi Liang!</h1>
-      <h2>Programming is my passion, Software Engineering is my pursuit, global warming is my concern.</h2>
+      <h1>Hi there, welcome to my website</h1>
+      <h2>I'm Kaiqi Liang</h2>
+      <h3>
+        <TextLoop interval={1500}>
+          {details.first}
+        </TextLoop>
+        {` is my `}
+        <TextLoop interval={1500}>
+          {details.second}
+        </TextLoop>
+      </h3>
     </Background>
   );
 };
